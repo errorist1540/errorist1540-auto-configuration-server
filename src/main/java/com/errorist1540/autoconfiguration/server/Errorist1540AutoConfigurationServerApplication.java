4fc4c6a41202861fcdcfaa9468df878c26b7db4d
YoonHyukJun <gurwns1540@gmail.com>
2021-12-28 02:00:23 +0900

    Init commit

---
+++ src/main/java/com/errorist1540/autoconfiguration/server/Errorist1540AutoConfigurationServerApplication.java
@@ -0,0 +1,13 @@
+package com.errorist1540.autoconfiguration.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Errorist1540AutoConfigurationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Errorist1540AutoConfigurationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/errorist1540/autoconfiguration/server/Errorist1540AutoConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.errorist1540.autoconfiguration.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Errorist1540AutoConfigurationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
